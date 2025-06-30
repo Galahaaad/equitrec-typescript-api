9626f3af2ff06ac075048153ff7b1886f756894b
Galah3ad <77021667+Galahaaad@users.noreply.github.com>
2025-06-30 11:13:56 +0200

    fix CORS pour la 32879827ème fois

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -9,7 +9,7 @@ export const setupMiddlewares = (app: express.Application): void => {
     app.use(cors({
         origin: config.nodeEnv === 'production'
             ? ['https://yourdomain.com'] // prévision : prod
-            : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4200/', 'http://localhost:8081','http://10.197.102.242:3001', 'http://10.197.102.242:8081/'],
+            : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4200', 'http://localhost:8081','http://10.197.102.242:3001', 'http://10.197.102.242:8081'],
         credentials: true,
     }));
 
