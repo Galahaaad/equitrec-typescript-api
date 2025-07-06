d42546c66722cf06850a4b3e29ec029504922ff2
Galah3ad <77021667+Galahaaad@users.noreply.github.com>
2025-07-06 16:51:51 +0200

    update role management for epreuves crud

--- src/routes/epreuves.ts
+++ src/routes/epreuves.ts
@@ -9,9 +9,9 @@ router.get('/:id', authenticateToken, EpreuveController.getEpreuveById);
 router.get('/:id/competitions', authenticateToken, EpreuveController.getEpreuveWithCompetitions);
 router.get('/:id/criteres', authenticateToken, EpreuveController.getEpreuveWithCriteres);
 router.get('/juge/:jugeId', authenticateToken, EpreuveController.getEpreuvesByJuge);
-router.post('/create', authenticateToken, requireJudgeRole, EpreuveController.createEpreuve);
-router.put('/:id', authenticateToken, requireJudgeRole, EpreuveController.updateEpreuve);
-router.delete('/:id', authenticateToken, requireJudgeRole, EpreuveController.deleteEpreuve);
+router.post('/create', authenticateToken, requireSuperAdmin, EpreuveController.createEpreuve);
+router.put('/:id', authenticateToken, requireSuperAdmin, EpreuveController.updateEpreuve);
+router.delete('/:id', authenticateToken, requireSuperAdmin, EpreuveController.deleteEpreuve);
 
 router.post('/:id/assign-competition', authenticateToken, requireSuperAdmin, EpreuveController.addCompetitionToEpreuve);
 router.delete('/:epreuveId/competitions/:competitionId', authenticateToken, requireSuperAdmin, EpreuveController.removeCompetitionFromEpreuve);
